f9cbbf6592f4534caa16ca55a7b680074fb71780
CosmosSolitarus <156846497+CosmosSolitarus@users.noreply.github.com>
2024-01-16 15:44:58 -0500

    Commit 1
    All of ArraySet
    All except last of ParallelArrayDictionary

--- src/utilities/ParallelArrayDictionary.java
+++ src/utilities/ParallelArrayDictionary.java
@@ -17,4 +17,72 @@ public class ParallelArrayDictionary<Key, Value> implements Map<Key, Value>
 		_values = new ArrayList<Value>();
 	}
 
+	@Override
+	public int size() {
+		return _keys.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return _keys.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return _keys.contains(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return _values.contains(value);
+	}
+
+	@Override
+	public Value get(Object key) {
+		return _values.get(_keys.indexOf(key));
+	}
+
+	@Override
+	public Value put(Key key, Value value) {
+		_keys.add(key);
+		
+		if (_values.add(value)) {
+			return value;
+		}
+		return null;
+	}
+
+	@Override
+	public Value remove(Object key) {
+		return _values.remove(_keys.indexOf(key));
+	}
+
+	@Override
+	public void putAll(Map<? extends Key, ? extends Value> m) {
+		for (Key key : m.keySet()) {
+			put(key, m.get(key));
+		}
+	}
+
+	@Override
+	public void clear() {
+		_keys.clear();
+		_values.clear();
+	}
+
+	@Override
+	public Set<Key> keySet() {
+		return _keys;
+	}
+
+	@Override
+	public Collection<Value> values() {
+		return _values;
+	}
+
+	@Override
+	public Set<Entry<Key, Value>> entrySet() {
+		return null;
+	}
+
 }
