1c95dc4d7c1d134d03366b58b0dd9a0422f37db7
eleanorbadgett <156824225+eleanorbadgett@users.noreply.github.com>
2024-01-18 15:40:42 -0500

    Update ArraySetTest.java

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -3,6 +3,11 @@ package utilities;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class ArraySetTest
 {
 	@Test
@@ -33,15 +38,50 @@ class ArraySetTest
 	@Test
 	void testAddAllCollectionOfQextendsE() {
 		ArraySet<Integer> list = new ArraySet<Integer>();
+		
+		int[]  set = new int[] {4,3,-6,17,932};
+		Set<Integer> set2 = new HashSet<Integer>(Arrays.asList(2,-8));
+		
+		
+		for(int i : set) {
+			list.add(i);
+		}
+		
+		list.addAll(set2);
+		//can addAll() valid elements
+		assertTrue (list.addAll(set2));
+		
+		//cannot add all duplicate elements
+		assertTrue(list.addAll(set2));
 	
-		assertFalse(true);
 	}
 
 	@Test
 	void testRetainAll() {
 		ArraySet<Integer> list = new ArraySet<Integer>();
 		
-		assertFalse(true);
+		int[] set = new int[]{2,-1,3,7,496,5};
+		
+		for(int i : set) {
+			list.add(i);
+		}
+		
+		Set<Integer> r = new HashSet<Integer>(Arrays.asList(7,3,-1));
+		//test valid retain elements
+		assertTrue(list.retainAll(r));
+		
+		ArraySet<Integer> list2 = new ArraySet<Integer>();
+		
+		for(int i : set) {
+			list2.add(i);
+		}
+		
+
+		Set<Integer> fail = new HashSet<Integer>(Arrays.asList(88,5,21));
+		
+		//does not retain invalid elmts 
+		assertTrue(list2.retainAll(fail));
+
 	}
 
 	@Test
